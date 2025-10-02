9c2bebb8c2385e830c0bb379fbcfd44ba34e015a
aadobely <164618044+aadobely@users.noreply.github.com>
2025-10-02 06:01:59 -0400

    Add Project 2: External JavaScript files ...

---
+++ Basic JavaScript Projects/Project2_external_js/JavaScript_File.js
@@ -0,0 +1,12 @@
+// This is an external JavaScript file
+// All JavaScript code goes here
+
+alert("Hello, World!");
+alert("This JavaScript is running from an external file!");
+
+// Let's add more JavaScript
+console.log("External JS file loaded successfully!");
+
+// Simple calculation
+var result = 10 + 20;
+alert("10 + 20 = " + result);
