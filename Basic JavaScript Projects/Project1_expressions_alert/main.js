2a19f5d2ad17bc909498b0c79cc42c352032242c
aadobely <164618044+aadobely@users.noreply.github.com>
2025-10-02 06:14:50 -0400

    Refactor Project 1: document.write() Method

---
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,8 @@
+// Using document.write() method to display text on the webpage
+document.write("Hello, world!");
+
+// You can also add HTML tags
+document.write("<h2>This text is written using document.write()</h2>");
+
+// Display multiple items
+document.write("<p>This method writes directly to the HTML document.</p>");
