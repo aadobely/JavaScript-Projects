66e0ab7603a7d51740602420e824f821f3b12478
aadobely <164618044+aadobely@users.noreply.github.com>
2025-10-02 06:44:26 -0400

    Complete variable, concatenation, and escape character assignments

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,50 @@
-// Alert method inside JS folder
-alert("This JavaScript file is organized in a JS folder!");
-alert("Good folder structure keeps projects organized!");
+// ===== ASSIGNMENT 1: Variable Assignment =====
+// Assign a string value to a variable
+var A = "This is a string";
+
+// Use document.write() to display the variable
+document.write(A);
+
+// Add spacing between assignments
+document.write("<br><br>");
+
+
+// ===== ASSIGNMENT 2: Multiple Variables Assignment =====
+// Assign multiple variables in one statement
+var firstName = "Hasan", lastName = "Adobely", age = 44, city = "Toronto";
+
+// Use document.write() to display one of the variables
+document.write("My first name is: " + firstName);
+
+// Add spacing
+document.write("<br><br>");
+
+
+// ===== ASSIGNMENT 3: Concatenation Assignment =====
+// Concatenate a string within document.write()
+document.write("Hello, " + "my name is " + firstName + " and I live in " + city + ".");
+
+// Add spacing
+document.write("<br><br>");
+
+
+// ===== ASSIGNMENT 4: Escape Character Assignment =====
+// String with apostrophes - using escape characters
+document.write("I\'m learning JavaScript at the Academy of Learning.");
+
+// Add spacing
+document.write("<br>");
+
+// String with quotation marks - using escape characters
+document.write("The instructor said, \"Practice makes perfect!\"");
+
+// Add spacing
+document.write("<br><br>");
+
+
+// ===== ASSIGNMENT 5: String Assignment =====
+// Assign a string value to a variable
+var greeting = "Welcome to my JavaScript learning journey!";
+
+// Use window.alert() to display the variable
+window.alert(greeting);
